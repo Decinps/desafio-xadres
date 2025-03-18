18d5b0b7f0eb46dfae46da02a8135e491ed4cfd2
Decinps <deciops22@gmail.com>
2025-03-18 23:07:20 +0000

    adcionadomovimento para a torre,bispo e a rainha. torre 5 casas para a direita; bispo 5 casas para a diagonal direita; rainha 8 casaspara a direita.

--- desafiox.c
+++ desafiox.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+int main (){
 
+    int i, j, k;
+j = 0;
+k = 0;
+
+    printf("Mover torre para direita\n\n");
+    
+// move torre 5 casas para a direita.
+    for ( i = 0; i < 5; i++)
+    {
+        printf("DIREITA\n");
+    }
+
+    printf("\n");
+//move o bispo para a diagonal direita.   
+         printf("Mover bispo para diagonal direita\n\n");
+    
+    while (j < 5)
+    {
+        printf("Cima direita \n");
+
+        j++;
+    }
+
+    printf("\n");    
+   
+         printf("Mover rainha para deireita oito casas\n\n");
+
+ //move a rainha para a direita 8 vezes.
+    do
+    {
+        printf("Direita\n");
+
+        k++;
+    } while (k <= 7);
+    
+    
+}
